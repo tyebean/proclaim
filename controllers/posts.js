2d2e320b741908971c2b0d6a85958cfde12a74ad
tyebean <flower.webster@gmail.com>
2022-03-06 00:44:31 -0500

    add tc index for ref

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,31 @@
 import { Post } from "../models/post.js"
 import { Comment } from "../models/comment.js"
 
+///posts (all posts)
 function index(req, res){
   console.log("index fun :)");
+  Post.find({})
+  .then(posts => {
+    res.render('posts/index',{
+      posts
+    })
+  })
+  .catch(err =>{
+    console.log(err, 'ctrlrs/idx/fun err');
+    res.redirect('/posts')
+  })
 }
 
+//specific post 
 function show (req, res){
   console.log("show me the function xD");
 }
 
+// new post (redirect to /posts)
 function create(req, res){
   console.log("create new fun");
+  
+
 }
 
 function deletePost (req, res){
