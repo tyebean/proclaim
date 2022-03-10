e305d55755734004f2a451581352a35b92ec47f3
tyebean <flower.webster@gmail.com>
2022-03-10 12:54:03 -0500

    style comments

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const commentSchema = new Schema({
   text: String,
-  time: String,
+  time: {type: Date, default: Date.now()},
   author: {type: Schema.Types.ObjectId, ref: "Profile"},
   post: {type: Schema.Types.ObjectId, ref: "Post"}
 }) 
